879cff9a7b1249f0da790e70f61dc9ceef61378d
fsy218 <999fsy218@gmail.com>
2019-09-25 22:29:26 +0900

    Update message.js

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,4 +1,4 @@
-$(document).on('turbolinks:load', function(){
+$(function(){
   function buildHTML(message) {
     var img = message.image ? `<img class= "message__lower-info__image" src=${message.image} >` : "";
     var html = `<div class="message" data-message="${message.id}">
