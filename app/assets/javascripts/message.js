5e048dc1f22b06b4ae56b6f08a227b348a631f02
fsy218 <999fsy218@gmail.com>
2019-09-27 01:07:30 +0900

    Update message.js

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -32,7 +32,6 @@ $(document).on('turbolinks:load', function(){
       contentType: false
     })
     .done(function(send_message){
-      debugger;
       if (send_message.length !== 0) {
       var html = buildHTML(send_message);
       $('.messages').append(html);
@@ -50,18 +49,9 @@ $(document).on('turbolinks:load', function(){
     })
   });
 
-  // var messageUpdateTime = -1;
-  // $(document).on('turbolinks:load', function() {
-  //   if (messageUpdateTime > 0) {clearInterval(messageUpdateTime);}
-  //   messageUpdateTime = (location.href.match(/\/groups\/\d+\/messages/)) ? setInterval(message_update, 5000) : -1;
-  // });
-
-
-  // function message_update() {
   var reloadMessages = function () {
-    if (window.location.href.match(/\/groups\/\d+\/messages/)){//今いるページのリンクが/groups/グループID/messagesのパスとマッチすれば以下を実行。
+    if (window.location.href.match(/\/groups\/\d+\/messages/)){
       var last_message_id = $('.message:last').data("id"); 
-    // var last_message_id = $(".message").last().data('id');
       $.ajax({
         url: 'api/messages',
         type: "GET",
@@ -69,7 +59,6 @@ $(document).on('turbolinks:load', function(){
         data: {id: last_message_id}
       })
       .done(function(messages) {
-        debugger;
         var insertHTML = '';
         messages.forEach(function(message) {
           insertHTML = buildHTML(message);
@@ -77,12 +66,8 @@ $(document).on('turbolinks:load', function(){
         });
         $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'slow')
       })
-      .fail(function(jqXHR, textStatus, errorThrown) {
+      .fail(function() {
         alert('自動更新に失敗しました');
-        // console.log("ajax通信に失敗しました");
-        // console.log("jqXHR          : " + jqXHR.status); // HTTPステータスが取得
-        // console.log("textStatus     : " + textStatus);    // タイムアウト、パースエラー
-        // console.log("errorThrown    : " + errorThrown.message); // 例外情報
       });
     }
   };
